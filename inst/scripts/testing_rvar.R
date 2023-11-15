8243eece006f409267cf7c868a928718172f40a1
theodds <theodds@gmail.com>
2023-11-14 18:56:22 -0600

    Made Test File

--- inst/scripts/testing_rvar.R
+++ inst/scripts/testing_rvar.R
@@ -1,11 +1,11 @@
-# Load --------------------------------------------------------------------
+## Load ----
 
 library(Batman)
 library(zeallot)
 library(rbart)
 library(Matrix)
 
-# Sim ---------------------------------------------------------------------
+## Sim ----
 
 set.seed(7658)
 
@@ -13,48 +13,51 @@ num_tree <- 50
 
 sim_data <- function(n,p) {
   X <- matrix(runif(n * p), nrow = n)
-  mu <- 4 * X[,1]^2
+  mu <- rep(0,n)
   sigma <- 0.2 * exp(2 * X[,1])
   Y <- mu + sigma * rnorm(n)
   return(list(X = X, Y = Y, mu = mu, sigma = sigma))
 }
 
 c(X,Y,mu,sigma) %<-% sim_data(500, 1)
-mu_Y <- mean(Y)
-sigma_Y <- sd(Y)
-Y_scale <- (Y - mu_Y) / sigma_Y
-## probs <- Matrix(data = c(1,0,0,1), nrow = 2, byrow = TRUE, sparse = TRUE)
-probs <- Matrix(data = 1, nrow = 1, byrow = TRUE, sparse = TRUE)
 
-empirical_lm <- lm(Y_scale ~ X)
+## Prepare data for fitting ----
+
+sigma_Y         <- sd(Y)
+Y_scale         <- Y / sigma_Y
+probs           <- Matrix(data = 1, nrow = 1, byrow = TRUE, sparse = TRUE)
+empirical_lm    <- lm(Y_scale ~ X)
 empirical_sigma <- sqrt(mean(empirical_lm$residuals^2))
 
-out <- VarBart(X, Y_scale, probs, 1.5 / sqrt(num_tree), .1 / sqrt(num_tree), 3, 3, num_tree, 4000, 1, 4000)
+## Fit the model, and rbart for comparison ----
+
+out <- RVarBart(X = X, Y = Y_scale,
+                probs = probs,
+                sigma_scale_log_tau = 1.5 / sqrt(num_tree),
+                shape_tau_0 = 3,
+                rate_tau_0 = 3,
+                num_trees = num_tree,
+                num_burn = 4000,
+                num_thin = 1,
+                num_save = 4000)
+
 outr <- rbart(X, Y, ntree = 50, ntreeh = 50, ndpost = 4000, nskip = 4000)
 
-pred_outr <- predict(outr)
-pred_outr$mmean
-pred_outr$smean
+## Collect predictions for rbart and RVarBart ----
 
-mu_hat <- colMeans(out$mu) * sigma_Y + mu_Y
+pred_outr <- predict(outr)
 sigma_hat <- colMeans(1/sqrt(out$tau)) * sigma_Y
 
-
+## Plot for comparison ----
 
 par(mfrow = c(1,2))
-
-plot(X, mu)
-o <- order(X)
-lines(X[o], mu_hat[o])
-lines(X[o], pred_outr$mmean[o], col = 'red')
 
 plot(X, sigma)
 lines(X[o], sigma_hat[o])
 lines(X[o], pred_outr$smean[o], col = 'red')
 
+## Measure Accuracy ----
+
 mean(abs(log(sigma_hat) - log(sigma)))
 mean(abs(log(pred_outr$smean) - log(sigma)))
-
-mean(abs(mu_hat - mu))
-mean(abs(pred_outr$mmean - mu))
 
