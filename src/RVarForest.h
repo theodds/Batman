dee3f40bbe6c0b2524a172662b7900b40163c2a7
theodds <theodds@gmail.com>
2023-11-14 18:05:48 -0600

    Updating RVarForest.h

--- src/RVarForest.h
+++ src/RVarForest.h
@@ -1,41 +1,36 @@
-#ifndef VAR_FOREST_H
-#define VAR_FOREST_H
+#ifndef RVAR_FOREST_H
+#define RVAR_FOREST_H
 
 #include <RcppArmadillo.h>
-#include "VarNode.h"
-#include "VarParams.h"
+#include "RVarNode.h"
+#include "RVarParams.h"
 #include "mcmc.h"
 
-struct VarForest {
-  std::vector<VarNode*> trees;
+struct RVarForest {
+  std::vector<RVarNode*> trees;
 
-  VarForest(int num_trees, TreeHypers* tree_hypers_, VarParams* var_params_)
+  RVarForest(int num_trees, TreeHypers* tree_hypers_, RVarParams* var_params_)
   {
     trees.resize(0);
     for(int t = 0; t < num_trees; t++) {
-      VarNode* n = new VarNode(tree_hypers_, var_params_);
+      VarNode* n = new RVarNode(tree_hypers_, var_params_);
       trees.push_back(n);
     }
   }
 
-  ~VarForest() {
+  ~RVarForest() {
     for(int t = 0; t < trees.size(); t++) {
       delete trees[t];
     }
   }
 };
 
-arma::mat PredictVar(std::vector<VarNode*>& forest, const arma::mat& X);
-void UpdateHypers(VarParams& var_params,
-                  std::vector<VarNode*>& trees,
-                  const VarData& data);
-void get_params(VarNode* n, std::vector<double>& mu, std::vector<double>& tau);
-void UpdateTau0(VarParams& var_params, VarData& data);
-void UpdateKappa(VarParams& var_params,
-            std::vector<double>& mu,
-            std::vector<double>& tau);
-void UpdateScaleLogTau(VarParams& var_params, std::vector<double>& tau);
-// void UpdateScaleLambda(MLogitParams& mlogit_params,
-//                        std::vector<MLogitNode*>& trees);
+arma::mat PredictVar(std::vector<RVarNode*>& forest, const arma::mat& X);
+void UpdateHypers(RVarParams& var_params,
+                  std::vector<RVarNode*>& trees,
+                  const RVarData& data);
+void get_params(RVarNode* n, std::vector<double>& tau);
+void UpdateTau0(RVarParams& var_params, RVarData& data);
+void UpdateScaleLogTau(RVarParams& var_params, std::vector<double>& tau);
 
 #endif
