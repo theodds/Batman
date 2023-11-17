c8b9637821d27cfaad48b8722be3055db9a2c01e
theodds <theodds@gmail.com>
2023-11-16 22:52:55 -0600

    Working on Quasi, won't compile until I finish this

--- src/QBinomData.h
+++ src/QBinomData.h
@@ -1,17 +1,21 @@
-#ifndef QPOIS_DATA_H
-#define QPOIS_DATA_H
+#ifndef QBINOM_DATA_H
+#define QBINOM_DATA_H
 
 #include <RcppArmadillo.h>
 
-struct QPoisData {
+struct QBinomData {
   arma::mat X;
   arma::vec Y;
+  arma::vec n;
   arma::vec lambda_hat;
+  arma::vec phi;
 
-  QPoisData(const arma::mat& X_, const arma::vec& Y_) {
+  QBinomData(const arma::mat& X_, const arma::vec& Y_, const arma::vec& n_) {
     X = X_;
     Y = Y_;
+    n = n_;
     lambda_hat = arma::zeros<arma::vec>(X.n_rows);
+    phi = arma::ones<arma::vec>(X.n_rows);
   }
 
 };
