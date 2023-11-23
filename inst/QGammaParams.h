84b844a827a1d2ea6b607f15d76f94b5411cc935
theodds <theodds@gmail.com>
2023-11-22 21:56:29 -0600

    Added QGamma files to inst

---
+++ inst/QGammaParams.h
@@ -0,0 +1,43 @@
+#ifndef QPOIS_PARAMS_H
+#define QPOIS_PARAMS_H
+
+#include <RcppArmadillo.h>
+#include "Node.h"
+
+struct QPoisParams {
+
+  double scale_lambda_0;
+  double sigma_scale_lambda;
+  double phi;
+
+  QPoisParams(double scale_lambda_0_, double scale_lambda_, double phi_) :
+    scale_lambda(scale_lambda_), 
+    phi(phi_),
+    sigma_scale_lambda(scale_lambda_),
+    scale_lambda_0(scale_lambda_0_) {
+    scale_lambda_to_ab();
+  }
+
+  double get_alpha() const {return alpha;}
+  double get_beta() const {return beta;}
+  double get_scale_lambda() const {return scale_lambda;}
+  double get_phi() const {return phi;}
+  void set_scale_lambda(double x) {
+    scale_lambda = x;
+    scale_lambda_to_ab();
+  }
+
+private:
+
+  double scale_lambda;
+  double alpha;
+  double beta;
+
+  void scale_lambda_to_ab() {
+    scale_lambda_to_alpha_beta(alpha, beta, scale_lambda);
+  }
+};
+
+
+
+#endif
--- inst/scripts/testing_qbin_beta.R
+++ inst/scripts/testing_qbin_beta.R
@@ -10,7 +10,6 @@ library(matrixStats)
 P <- 10
 N <- 250
 num_tree <- 50
-num_within <- 10
 num_burn <- 4000
 num_thin <- 1
 num_save <- 4000
@@ -19,14 +18,14 @@ num_save <- 4000
 
 sigma <- 0.1
 set.seed(1234)
-sim_fried_mlogit <- function(n,p,sigma) {
+sim_fried_mlogit <- function(n, p, sigma, phi = 0.5) {
   f <- function(x)
     10 * sin(pi * x[,1] * x[,2]) + 20 * (x[,3]-0.5)^2 + 10 * x[,4] + 5 * x[,5]
-  
+  s <- 1 / phi - 1
   X <- matrix(runif(n*p), nrow = n)
   p <- pnorm(sigma * (f(X) - 12))
-  Y <- 2 * rbinom(n = n, size = num_within, prob = p / 2)
-  n <- rep(num_within, length(Y))
+  Y <- rbeta(n = n, shape1 = s * p, shape2 = s * (1 - p))
+  n <- rep(1, length(Y))
   
   return(list(X = X, Y = Y, p = p, n = n))
 }
@@ -51,44 +50,11 @@ quasi_fit <-
     num_save = num_save
   )
 
-## Prepping data for dbarts ----
-
-convert_y <- function(x, y, n) {
-  yy <- numeric(n)
-  yy[1:y] <- 1
-  xx <- matrix(nrow = 0, ncol = length(x))
-  for(i in 1:length(x)) {
-    xx <- rbind(xx, x)
-  }
-  return(data.frame(X = xx, Y = yy))
-}
-
-expand_df <- map_df(.x = 1:nrow(X), .f = \(i) convert_y(X[i,], Y[i], n[i]))
-
-X_expand <- as.matrix(expand_df %>% select(-Y))
-Y_expand <- expand_df$Y
-
-## Fitting dbarts ----
-
-dbarts_bin <- bart(x.train = X_expand, y.train = Y_expand, ntree = num_tree, 
-                   ndpost = 4000, nskip = 4000)
-
-p_hat_1 <- colMeans(plogis(quasi_fit$lambda))
-p_hat_2 <- colMeans(pnorm(dbarts_bin$yhat.train[, seq(1,nrow(X_expand),n[1])]))
-
-plot(p, p_hat_1)
-plot(p, p_hat_2)
-
--2*sum(p * log(p_hat_1) + (1 - p) * log(1 - p_hat_1))
--2*sum(p * log(p_hat_2) + (1 - p) * log(1 - p_hat_2))
-
-## Comparing standard errors ----
+## Evaluate Output ----
 
 p_1 <- plogis(quasi_fit$lambda)
-p_2 <- pnorm(dbarts_bin$yhat.train[, seq(1, nrow(X_expand), n[1])])
 
 par(mfrow = c(1,2))
-plot(colSds(p_1), colSds(p_2))
-plot(density(colSds(p_1)), lwd = 2)
-lines(density(colSds(p_2)), col = 3, lty = 2, lwd = 2)
+plot(colMeans(p_1), p)
+hist(quasi_fit$phi)
 par(mfrow = c(1,1))
