7de29ca568d1e435a1f56da14e5b7c5a6175b5a4
theodds <theodds@gmail.com>
2024-06-06 22:42:36 -0500

    Update NPHSBPForest.h

--- src/Testing/NPHSBPForest.h
+++ src/Testing/NPHSBPForest.h
@@ -1,34 +1,34 @@
-#ifndef COX_NPHO_FOREST_H
-#define COX_NPHO_FOREST_H
+#ifndef NPHSBP_FOREST_H
+#define NPHSBP_FOREST_H
 
 #include <RcppArmadillo.h>
-#include "CoxNPHONode.h"
-#include "CoxNPHOParams.h"
+#include "NPHSBPNode.h"
+#include "NPHSBPParams.h"
 #include "mcmc.h"
 
-struct CoxNPHOForest {
-  std::vector<CoxNPHONode*> trees;
+struct NPHSBPForest {
+  std::vector<NPHSBPNode*> trees;
 
-  CoxNPHOForest(int num_trees, TreeHypers* tree_hypers, CoxNPHOParams* cox_params) {
+  NPHSBPForest(int num_trees, TreeHypers* tree_hypers, NPHSBPParams* cox_params) {
     trees.resize(0);
     for(int t = 0; t < num_trees; t++) {
-      CoxNPHONode* n = new CoxNPHONode(tree_hypers, cox_params);
+      NPHSBPNode* n = new NPHSBPNode(tree_hypers, cox_params);
       trees.push_back(n);
     }
   }
 
-  ~CoxNPHOForest() {
+  ~NPHSBPForest() {
     for(int t = 0; t < trees.size(); t++) {
       delete trees[t];
     }
   }
 };
 
-arma::mat PredictCox(std::vector<CoxNPHONode*>& forest, const arma::mat& X, int K);
-void UpdateHypers(CoxNPHOParams& cox_params,
-                  std::vector<CoxNPHONode*>& trees,
-                  CoxNPHOData& data);
-void get_params(CoxNPHONode* n, std::vector<double>& lambda);
-void UpdateScaleLambda(CoxNPHOParams& cox_params, std::vector<double>& lambda);
+arma::mat PredictCox(std::vector<NPHSBPNode*>& forest, const arma::mat& X, int K);
+void UpdateHypers(NPHSBPParams& cox_params,
+                  std::vector<NPHSBPNode*>& trees,
+                  NPHSBPData& data);
+void get_params(NPHSBPNode* n, std::vector<double>& lambda);
+void UpdateScaleLambda(NPHSBPParams& cox_params, std::vector<double>& lambda);
 
 #endif
